e35ece8e6926d843ac5c0003bd84571fd571d7ac
ravi <rpravi.in@gmail.com>
2019-04-16 23:44:32 +0530

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Demo d=new Demo();
+		d.appConfig();
+
+	}
+	
+	public void appConfig(){
+		System.out.println("appCongig");
+	}
+
+}
