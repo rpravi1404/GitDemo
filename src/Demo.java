0a0ad217d975eefac52a0f4eb1f38a2246451411
ravi <rpravi.in@gmail.com>
2019-04-17 00:04:07 +0530

    Second Commit

--- src/Demo.java
+++ src/Demo.java
@@ -4,11 +4,16 @@ public class Demo {
 	public static void main(String[] args) {
 		Demo d=new Demo();
 		d.appConfig();
+		d.appConfig1();
 
 	}
 	
 	public void appConfig(){
 		System.out.println("appCongig");
+	}
+	
+	public void appConfig1(){
+		System.out.println("appCongig1");
 	}
 
 }
